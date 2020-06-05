e46526ad8d860d2ad5c06daed4e3e7b6f3506640
Jesse Gould <jesse.gould@codeimmersives.com>
2020-06-05 11:28:47 -0400

    adds context for user

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-// import { Consumer } from "./components/Context/Context";
+import { Consumer } from "./components/Context/Context";
 // import PrivateRoute from "./components/PrivateRoute/PrivateRoute"
-// import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 const Home = React.lazy(() => import("./components/Home/Home"));
 const Signin = React.lazy(() => import("./components/Signin/Signin"));
@@ -11,18 +11,23 @@ const Signup = React.lazy(() => import("./components/Signup/Signup"));
 export default class MainRouter extends Component {
   render() {
     return (
-      
-            <>
-              {/* <Navbar/> */}
+      <Consumer>
+      {({ dispatch }) => {
+        return (
+          <>
+          <Navbar/>
 
-              <Switch>
-                <Route exact path="/sign-up" component={Signup} />
-                {/* <Route exact path="/sign-in" component={Signin} /> */}
-                <Route exact path="/" component={Home} />
-                
-                {/* <PrivateRoute exact path="/expense" component={Expense} /> */}
-              </Switch>
-            </>
+          <Switch>
+            <Route exact path="/sign-up" component={Signup} />
+            <Route exact path="/sign-in" component={Signin} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </>
+
+        )
+      }}
+      </Consumer>  
+           
           );
   }
 }
